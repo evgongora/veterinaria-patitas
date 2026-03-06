a9f74e2876b25737c560164a7a620980ae2f6e85
Alondra-Matamoros <alondramatamoros1@gmail.com>
2026-03-06 01:08:43 -0600

    feat: modulo mascotas y citas

--- js/validaciones.js
+++ js/validaciones.js
@@ -1,3 +1,16 @@
-/**
- * Veterinaria Patitas - Validaciones
- */
+function campoVacio(valor) {
+    return valor === null || valor.trim() === "";
+}
+
+function validarCamposRequeridos(campos) {
+    for (let i = 0; i < campos.length; i++) {
+        if (campoVacio(campos[i].value)) {
+            return false;
+        }
+    }
+    return true;
+}
+
+function validarHoras(horaInicio, horaFin) {
+    return horaInicio < horaFin;
+}
